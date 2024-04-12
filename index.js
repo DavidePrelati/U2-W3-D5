55eaf28b98c4cc0beda433beb124056fe8bb3a8b
DavidePrelati <davide.prelati@gmail.com>
2024-04-12 15:08:07 +0200

    index js e html

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
-fetch('https://striveschool-api.herokuapp.com/api/product/', {
-        method: 'GET'
+const URL = "https://striveschool-api.herokuapp.com/api/product/";
+
+window.addEventListener("DOMContentLoaded", () => {
+    fetch(URL, {
+        method: "GET",
+        headers: {
+            Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZWI4MTdmMzA0NjAwMWFlNTlmNmMiLCJpYXQiOjE3MTI5MjA5MDMsImV4cCI6MTcxNDEzMDUwM30.uu50kHV7QgEfBYDulsYPFV7jvPOjtAHV7RSqaNDtcmM",
+        },
     })
-    .then((response) => {
-        console.log(response)
-
-        if (response.ok) {
-            
-            return response.json()
-        } else {
-            
-            if (response.status === 400) { throw new Error("Bad Request") }
-            if (response.status === 401) { throw new Error("Unauthorized") }
-            if (response.status === 403) { throw new Error("Forbidden") }
-            if (response.status === 404) { throw new Error("Not Found") }
-            if (response.status === 500) { throw new Error("Server Error") }
-
-            throw new Error("Generic Fetch Error")
-
-        }
-    })
-        .then(products => {
-            const row = document.getElementById("products-container")
-            libraryData.forEach(book => {
-
-                const col = document.createElement("col")
-                col.classList.add("col")
-                const card = document.createElement("div")
-                card.classList.add("card")
-
-                card.innerHTML =  `
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">${product.name}</h5>
-                            <p class="card-text">${product.description}</p>
-                            <p class="card-text">Prezzo: ${product.price}</p>
-                            <img src="${product.imageUrl}" class="card-img-top">
-                        </div>
-                    </div>
-                `;
-                productsContainer.appendChild(productCard);
+        .then((resp) => {
+            if (resp.ok) {
+                return resp.json();
+            } else {
+                throw new Error("Error getting the images");
+            }
+        })
+        .then((products) => {
+            const row = document.getElementById("row");
+            console.log(row);
+            console.log(products);
+            products.forEach((product) => {
+                const col = document.createElement("div");
+                col.classList.add("col");
+                col.innerHTML = `<div class="card"> 
+                            <img src=${product.imageUrl} class="card-img-top" alt="...">
+                            <div class="card-body position-relative border border-2"> 
+                            <p class="card-text">${product.name}</p> 
+                            <p class="card-text small">By ${product.brand}</p>
+                            <p class="card-text">€${product.price}</p> 
+                            <a href="details.html?appId=${product._id}" class="btn btn-primary">Details</a>
+                            <a href="backoffice.html?appId=${product._id}" class="btn btn-primary">Edit</a>
+                            </div>
+                            </div>`;
+                row.appendChild(col);
             });
         })
-        .catch(error => {
-            console.error('Errore durante la richiesta GET:', error);
+        .catch((err) => {
+            console.log(err);
         });
+});
